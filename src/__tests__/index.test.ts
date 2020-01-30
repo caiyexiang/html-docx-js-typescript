f5f029c12113807db272eeac7783f0bcd4a6db51
caiyexiang <caiyexiang1@live.com>
2020-01-31 00:43:15 +0800

    test(index.test.ts): add new test case

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,4 @@
 import { asBlob } from '../index'
-const fs = require('fs')
 
 const HTML_CASE = `<!DOCTYPE html>
 <html lang="en">
@@ -27,6 +26,16 @@ describe('arguments test', () => {
   })
   test(`html,options={orientation,margins}`, async () => {
     const data = await asBlob(HTML_CASE, { orientation: 'portrait', margins: { left: 1000 } })
+    expect(data).toBeInstanceOf(Blob)
+  })
+  test(`html,options:Options`, async () => {
+    const opt = {
+      orientation: 'portrait' as const,
+      margins: {
+        bottom: 10,
+      },
+    }
+    const data = await asBlob(HTML_CASE, opt)
     expect(data).toBeInstanceOf(Blob)
   })
 })
